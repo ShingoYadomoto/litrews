26decb43c94ee308044388617b6e278f583ef3f4
ShingoYadomoto <shingo.yadomoto@gmail.com>
2018-09-09 17:30:15 +0900

    curl先のURL指定忘れを修正

--- src/handler/home.go
+++ src/handler/home.go
@@ -5,15 +5,18 @@ import (
 	"time"
 
 	"github.com/ShingoYadomoto/litrews/src/api"
+	"github.com/ShingoYadomoto/litrews/src/context"
 	"github.com/ShingoYadomoto/litrews/src/job"
 	"github.com/bamzi/jobrunner"
 	"github.com/labstack/echo"
 )
 
 func Home(c echo.Context) (err error) {
+	conf := c.(*context.CustomContext).GetConfig()
+
 	topics := api.GetAllTopics()
 	jobrunner.Start()
-	jobrunner.In(28*time.Minute, job.Curl{})
+	jobrunner.In(2*time.Second, job.Curl{conf.App.URL})
 
 	return c.Render(http.StatusOK, "home", map[string]interface{}{
 		"topics": topics,
