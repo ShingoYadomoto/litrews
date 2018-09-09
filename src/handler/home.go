16fe4bbf7e475518b28d4e1e709dbb50f3e1f1fd
ShingoYadomoto <shingo.yadomoto@gmail.com>
2018-09-09 22:13:00 +0900

    jobをデプロイ時にスケジューリング

--- src/handler/home.go
+++ src/handler/home.go
@@ -3,11 +3,7 @@ package handler
 import (
 	"net/http"
 
-	"time"
-
 	"github.com/ShingoYadomoto/litrews/src/api"
-	"github.com/ShingoYadomoto/litrews/src/context"
-	"github.com/ShingoYadomoto/litrews/src/job"
 	"github.com/bamzi/jobrunner"
 	"github.com/labstack/echo"
 )
@@ -22,10 +18,5 @@ func Home(c echo.Context) (err error) {
 }
 
 func Jobjson(c echo.Context) (err error) {
-	conf := c.(*context.CustomContext).GetConfig()
-
-	jobrunner.Start()
-	jobrunner.In(25*time.Minute, job.Curl{conf.App.URL})
-
 	return c.JSON(200, jobrunner.StatusJson())
 }
--- src/job/herokuUp.go
+++ src/job/herokuUp.go
@@ -3,11 +3,17 @@ package job
 import (
 	"net/http"
 
+	"github.com/bamzi/jobrunner"
 	"github.com/labstack/gommon/log"
 )
 
 type Curl struct {
 	URL string
+}
+
+func HerokuUp(url string) {
+	jobrunner.Start()
+	jobrunner.Schedule("@every 3s", Curl{url})
 }
 
 func (e Curl) Run() {
